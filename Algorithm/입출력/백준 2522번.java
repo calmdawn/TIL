eb47d36f55372b91f51a91951e6b8fcf44a1d352
JunHyeong <46203866+calmdawn@users.noreply.github.com>
2020-09-30 03:30:50 +0900

    Create 백준 2522번.java

---
+++ Algorithm/입출력/백준 2522번.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int N = scan.nextInt();
+		scan.close();
+
+		for (int i = 1; i <= N; i++) {
+
+			for (int k = 0; k < N - i; k++) { // 공백 넣기
+				System.out.print(" ");
+			}
+
+			for (int j = 0; j < i; j++) { // 별 넣기
+				System.out.print("*");
+			}
+
+			System.out.println();
+
+		}
+
+		for (int i = N - 1; i > 0; i--) {
+
+			for (int k = 0; k < N - i; k++) { // 공백 넣기
+				System.out.print(" ");
+			}
+
+			for (int j = 0; j < i; j++) { // 별 넣기
+				System.out.print("*");
+			}
+
+			System.out.println();
+
+		}
+	}
+}
