3174ad7a2ae65cb3ac28246e8a859bfea9e887c6
JunHyeong <46203866+calmdawn@users.noreply.github.com>
2020-10-06 02:54:45 +0900

    Update 4. 백준 4949번.java

--- Algorithm/스택(Stack)/4. 백준 4949번.java
+++ Algorithm/스택(Stack)/4. 백준 4949번.java
@@ -8,55 +8,56 @@ public class Main {
 
 		Stack<String> stack = new Stack<>();
 
-		String str = scan.nextLine();
+		String str = "";
 		String oneChar = "";
-		String result = "";
 
-		for (int i = 0; i < str.length(); i++) {
+		while (!(str = scan.nextLine()).equals(",")) {
 
-			oneChar = String.valueOf(str.charAt(i));
-
-			if (oneChar.equals("[")) {
-				stack.add(oneChar);
-			} else if (oneChar.equals("(")) {
-				stack.add(oneChar);
-			} else if (oneChar.equals(")")) {
-				stack.add(oneChar);
-			} else if (oneChar.equals("]")) {
-				stack.add(oneChar);
-			}
-
-		}
-
-		while (!stack.empty()) {
-
-			String checkChar = stack.peek();
-
-			if (checkChar.equals("]")) {
-				stack.pop();
-				checkChar = stack.peek();
-				if (checkChar.equals("[")) {
-					stack.pop();
-				} else {
-					result = "NO";
-					break;
-				}
-			} else if (checkChar.equals(")")) {
-				stack.pop();
-				checkChar = stack.peek();
-				if (checkChar.equals("(")) {
-					stack.pop();
-				} else {
-					result = "NO";
-					break;
-				}
-			}
+			stack.clear();
 			
-			result = "YES";
+
+			if (str.equals(".")) { // 입력의 종료조건인 . 이 들어온 경우
+				break;
+
+			}
+
+			for (int i = 0; i < str.length(); i++) { // 입력 종료조건이아닌 경우
+
+				oneChar = String.valueOf(str.charAt(i));
+
+				if (oneChar.equals("[")) {
+					stack.push(oneChar);
+				} else if (oneChar.equals("(")) { // [ 또는 ( 이면 스택에 추가
+					stack.push(oneChar);
+				} else if (oneChar.equals(")")) {
+					if (stack.empty()) {
+						stack.push(oneChar);
+						break;
+					} else if (stack.peek().equals("[")) {
+
+						break;
+					}
+					stack.pop();
+				} else if (oneChar.equals("]")) {
+					if (stack.empty()) {
+						stack.push(oneChar);
+						break;
+					} else if (stack.peek().equals("(")) {
+
+						break;
+					}
+					stack.pop();
+				}
+			}
+
+			if (!stack.empty()) { // 왼쪽 괄호만 있는 경우
+				System.out.println("NO");
+			} else {
+				System.out.println("YES");
+			}
 
 		}
-
-		System.out.println(result);
-
+		scan.close();
 	}
+
 }
