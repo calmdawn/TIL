1b6c72275e85a02ef0b7c93027469ee34b70d20b
JunHyeong <46203866+calmdawn@users.noreply.github.com>
2020-10-05 23:29:07 +0900

    Create 4. 백준 4949번.java

---
+++ Algorithm/스택(Stack)/4. 백준 4949번.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		Stack<String> stack = new Stack<>();
+
+		String str = scan.nextLine();
+		String oneChar = "";
+		String result = "";
+
+		for (int i = 0; i < str.length(); i++) {
+
+			oneChar = String.valueOf(str.charAt(i));
+
+			if (oneChar.equals("[")) {
+				stack.add(oneChar);
+			} else if (oneChar.equals("(")) {
+				stack.add(oneChar);
+			} else if (oneChar.equals(")")) {
+				stack.add(oneChar);
+			} else if (oneChar.equals("]")) {
+				stack.add(oneChar);
+			}
+
+		}
+
+		while (!stack.empty()) {
+
+			String checkChar = stack.peek();
+
+			if (checkChar.equals("]")) {
+				stack.pop();
+				if (checkChar.equals("[")) {
+
+				} else {
+					result = "NO";
+					break;
+				}
+			} else if (checkChar.equals(")")) {
+				if (checkChar.equals("(")) {
+
+				} else {
+					result = "NO";
+					break;
+				}
+			}
+
+			result = "YES";
+
+		}
+
+		System.out.println(result);
+
+	}
+}
